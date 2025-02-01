d06df1051f3aa7ab3870018889fa62790cef4355
nadeenanwar4 <nadeenanwar4@gmail.com>
2025-02-01 18:41:08 +0200

    Creating News Service

---
+++ lib/services/general_news_service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/news_model.dart';
+
+class NewsService {
+  final dio = Dio();
+  Future<List<dynamic>> getGeneralNews() async {
+    Response response = await dio.get(
+        "https://newsapi.org/v2/everything?apiKey=3bd05334b10143609d0d26204f6374a0&language=ar&q=egypt");
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articlesData = jsonData["articles"];
+    List<NewsModel> articles = [];
+    for (var article in articlesData) {
+      articles.add(NewsModel(
+          subTitle: article["description"],
+          imgPath: article["urlToImage"],
+          title: article["title"]));
+    }
+    return articles;
+    // return articlesData;
+  }
+}
