0d14e6c19ef63a49454354c55f0ea1a3db3ec77f
nadeenanwar4 <nadeenanwar4@gmail.com>
2025-02-04 21:58:59 +0200

    Set App Routes

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,6 +1,10 @@
 class CategoryModel {
   final String imgPath;
   final String title;
+  final String categoryPagePath;
 
-  const CategoryModel({required this.imgPath, required this.title});
+  const CategoryModel(
+      {required this.categoryPagePath,
+      required this.imgPath,
+      required this.title});
 }
--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/models/category_model.dart';
-import 'package:weather_app/widgets/category_container.dart';
+import 'package:weather_app/widgets/category_card.dart';
 
 class CategoryList extends StatelessWidget {
   final List<CategoryModel> categories = const [
-    CategoryModel(imgPath: "assets/images/business.jpeg", title: "Business"),
     CategoryModel(
-        imgPath: "assets/images/entertainment.jpeg", title: "Entertainment"),
-    CategoryModel(imgPath: "assets/images/general.jpeg", title: "General"),
-    CategoryModel(imgPath: "assets/images/health.jpeg", title: "Health"),
-    CategoryModel(imgPath: "assets/images/science.jpeg", title: "Science"),
-    CategoryModel(imgPath: "assets/images/sports.jpg", title: "Sports"),
+        imgPath: "assets/images/business.jpeg",
+        title: "Business",
+        categoryPagePath: '/businessView'),
     CategoryModel(
-        imgPath: "assets/images/technology.jpeg", title: "Technology"),
+        imgPath: "assets/images/entertainment.jpeg",
+        title: "Entertainment",
+        categoryPagePath: '/entertainmentView'),
+    CategoryModel(
+        imgPath: "assets/images/health.jpeg",
+        title: "Health",
+        categoryPagePath: '/healthView'),
+    CategoryModel(
+        imgPath: "assets/images/science.jpeg",
+        title: "Science",
+        categoryPagePath: '/scienceView'),
+    CategoryModel(
+        imgPath: "assets/images/sports.jpg",
+        title: "Sports",
+        categoryPagePath: '/sportsView'),
+    CategoryModel(
+        imgPath: "assets/images/technology.jpeg",
+        title: "Technology",
+        categoryPagePath: '/technologyView'),
+    CategoryModel(
+        imgPath: "assets/images/general.jpeg",
+        title: "General",
+        categoryPagePath: '/generalView'),
   ];
 
   const CategoryList({
