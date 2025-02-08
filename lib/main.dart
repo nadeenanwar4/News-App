b11ef774852836f5458f2caccec56946db2ec9c1
nadeenanwar4 <nadeenanwar4@gmail.com>
2025-02-08 22:24:38 +0200

    Fixing dotnev issue

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:weather_app/views/home_view.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load();
   runApp(const NewsApp());
 }
 
